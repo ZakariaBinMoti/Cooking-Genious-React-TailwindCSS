f1f864c6d93a3ac26569f9baf22d3a8b6954bcf1
Zakaria Bin Moti <zakaria.binmoti@gmail.com>
2024-03-19 02:02:57 +0600

    added a footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react'
 import './App.css'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Recipes from './components/Recipes/Recipes'
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header></Header>
       <Recipes></Recipes>
+      <Footer></Footer>
     </>
   )
 }
